916616e23317f71c9fd7fb9846637d125b623e60
SerhioKhavanski <serhiobladestone@gmail.com>
2026-05-18 14:58:31 +0300

    learn use effect with deps

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   const[selectedTaskId,setSelectedTaskId] = useState(null)
   const[selectedTask,setSelectedTask] = useState(null)
   const[tasks, setTasks]=useState(null)
+  const[boardId,setBoardId]=useState(null)
   useEffect(()=>{
     fetch('https://trelly.it-incubator.app/api/1.0/boards/tasks',{
       headers:{
@@ -15,6 +16,21 @@ function App() {
       setTasks(json.data)
     })
   },[])
+
+  useEffect(()=>{
+    if(boardId===null&&selectedTaskId===null){
+      return
+    }
+    fetch(`https://trelly.it-incubator.app/api/1.0/boards/${boardId}/tasks/${selectedTaskId}`,{
+        headers:{
+            'api-key':'01774c8c-0fec-4605-8f76-5d575b387023'
+        }
+      }).then(res=>res.json())
+      .then(json=>{
+        const {data}= json
+        setSelectedTask(data)
+      })
+  },[selectedTaskId])
 
   return (
     
@@ -32,20 +48,11 @@ function App() {
                 <span>На сегодня тасков нет</span>:        
                     <ul className="tasks-lists">
                       {tasks.map(task =>(
-                        <li onClick={()=>{
-                          setSelectedTaskId(task.id)
-                          fetch(`https://trelly.it-incubator.app/api/1.0/boards/${task.attributes.boardId}/tasks/${task.id}`,{
-                            headers:{
-                                'api-key':'01774c8c-0fec-4605-8f76-5d575b387023'
-                            }
-                          }).then(res=>res.json())
-                          .then(json=>{
-                            const {data}= json
-                            setSelectedTask(data)
-                            console.log(data);
-                          })
-                        } 
-                          } 
+                        <li onClick={()=>
+                          {
+                            setSelectedTaskId(task.id)
+                            setBoardId(task.attributes.boardId)
+                          }} 
                           key={task.id} 
                           className = {task.attributes.priority===0
                           ?'priority-0':task.attributes.priority===1
@@ -80,20 +87,6 @@ function App() {
                     <li>Описане: {selectedTask.attributes.description===null?'Описание отсутствует ':selectedTask.attributes.description}</li>
                   </ul>}
 
-                {/* {
-
-                  selectedTaskId === null
-                  ?<span>Вы не выбрали таск</span>
-                  :selectedTask===null 
-                  ?<span>подгружаем инфо</span>
-                  :selectedTask.id!==selectedTaskId
-                  ?<span>подгружаем инфо</span>
-                  :<ul className="desc"> 
-                    <li>Название таска: {selectedTask.attributes.title}</li>
-                    <li>Название доски: {selectedTask.attributes.boardTitle}</li>
-                    <li>Описане: {selectedTask.attributes.description===null?'Описание отсутствует ':selectedTask.attributes.description}</li>
-                  </ul>
-                } */}
               </div>
         </div>
     </>
